3a7a13e567440e6b60beaf153dff9b63b358bb66
PH <phcloudgemini@gmail.com>
2025-05-13 22:26:36 -0300

    Adicionando dadosdoUsuario02.java

---
+++ dadosdoUsuario02.java
@@ -0,0 +1,24 @@
+/**
+ * author Paulo Henrique Cabral
+ */
+
+//Importanto a biblioteca Scanner
+import java.util.Scanner;
+
+public class dadosdoUsuario02 {
+    public static void main(String [] args){
+        System.out.println("Olá, boa noite");
+
+
+        Scanner nomeUsuario = new Scanner(System.in);
+
+        System.out.println("Qual é o seu nome ?");
+
+         String nome = nomeUsuario.nextLine();
+
+         System.out.println("Seja bem-vindo " +nome);
+
+
+
+    }
+}
